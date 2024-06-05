082e9302c08239e6db194dd9449dcfbd5aa1e316
PriyaKatira <152061800+PriyaKatira@users.noreply.github.com>
2024-06-05 17:41:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmil = "priyakatira26@gmil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 //not alloved.
+
+
+accountEmil = "pk@kp.com"
+accountPassword = "262626"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope.
+*/
+
+
+console.table([accountId, accountEmil, accountPassword, accountCity, accountState]);
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Priya"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;  false => 0
+
+let isLoggedIn = "Priya"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "Priya" => true;
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof(stringNumber));
